993228086253ab85fbe6a7d827c6d6626e251d87
Aoo <suradechoommee@gmail.com>
2016-08-08 17:04:29 +0700

    Finish 8 Aug2016

--- app/src/main/java/rmutt/sorawith/suradech/sensorsystemdesign/MainActivity.java
+++ app/src/main/java/rmutt/sorawith/suradech/sensorsystemdesign/MainActivity.java
@@ -1,6 +1,7 @@
 package rmutt.sorawith.suradech.sensorsystemdesign;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -36,10 +37,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.imageView2:
-                startActivity(new Intent(MainActivity.this,SwitchActivity.class));
+                //startActivity(new Intent(MainActivity.this,SwitchActivity.class));
+
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://192.168.1.5"));
+                startActivity(intent);
+
                 break;
             case R.id.imageView3:
-                startActivity(new Intent(MainActivity.this,CarParkActivity.class));
+                //startActivity(new Intent(MainActivity.this,CarParkActivity.class));
+                Intent intent1 = new Intent(Intent.ACTION_VIEW);
+                intent1.setData(Uri.parse("http://192.168.1.12"));
+                startActivity(intent1);
+
                 break;
             case R.id.imageView4:
                 startActivity(new Intent(MainActivity.this,TempActivity.class));
